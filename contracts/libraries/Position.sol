35438ae7a24f716192153405d19bfa8a36ec364c
adair <adairchan.dream@gmail.com>
2024-08-21 11:39:27 +0800

    feat: uniswap-v3 upgrade solidity version

--- contracts/libraries/Position.sol
+++ contracts/libraries/Position.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity >=0.5.0 <0.8.0;
-
+pragma solidity ^0.8.20;
 import './FullMath.sol';
 import './FixedPoint128.sol';
 import './LiquidityMath.sol';
